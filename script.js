96e65370f9c88c1c7919d1ae2a2140eee780b1a1
Muhammad Zohaib Ul Hassan <zohaibfas@gmail.com>
2023-08-30 10:34:34 +0500

    project completed
    this is a simple portfolio website with a minimalist design

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-const hamburger = document.querySelector(".z-50")
-const menu = document.getElementById('menu')
+const hamburger = document.querySelector("#hamburger")
+const menu = document.querySelector("#menu")
+const moon = document.querySelector("#moon")
+const body = document.querySelector("body")
+const hLinks = document.querySelectorAll("#hLink")
 
-hamburger.addEventListener('click',()=>{
-    menu.classList.toggle('hidden')
+hamburger.addEventListener("click", ()=>{
+  menu.classList.toggle("hidden")
+  hamburger.classList.toggle("bg-white")
+})
+
+hLinks.forEach(link=>{
+  link.addEventListener("click", ()=>{
+    menu.classList.toggle("hidden")
+    hamburger.classList.toggle("bg-white")
+  })
+})
+
+moon.addEventListener("click", ()=>{
+  body.classList.toggle("dark")
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ module.exports = {
   theme: {
     extend: {
       screens:{
-        'sm':"480px"
+        sm:"480px",
+        md:"768px",
+        lg:"1024px",
+        xl:"1280px",
       },
       spacing:{
         'big':"48rem"
